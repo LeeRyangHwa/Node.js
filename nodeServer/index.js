f82ffd3321f596eb3c44c6aee202286f096be5a7
LeeRyangHwa <41534467+LeeRyangHwa@users.noreply.github.com>
2020-01-01 23:56:24 +0900

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,7 +1,7 @@
 //http모듈 사용
 var http = require('http');
 
-//http모듈로 서버를 생성한다.  
+//http모듈로 서버를 생성
 //
 var server = http.createServer(function(request,response){
     response.writeHead(200, {'Content-Type':'text/html'});
