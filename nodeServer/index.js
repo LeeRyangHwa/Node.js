a5859ed86a563a7ee773dc2f51608bb41bcedba5
이량화 <ilyanghwa@ilyanghwaui-MacBookPro.local>
2020-01-02 01:07:49 +0900

    com

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -4,9 +4,12 @@ var http = require('http');
 //http모듈로 서버를 생성
 var server = http.createServer(function(request,response){
     response.writeHead(200, {'Content-Type':'text/html'});
+    response.write('<h1>시작</h1>')
     response.end('Hello node.js!!');
+    
+
 });
-//8080포트를 가진 서버실행한다.
+//8080포트 서버실행
 server.listen(8080, function(){
     console.log('Server is running...');
 });
