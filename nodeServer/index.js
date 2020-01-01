18bea74b5631ccd88487bd5b0ced6d5a8094ecd7
이량화 <ilyanghwa@ilyanghwaui-MacBookPro.local>
2020-01-01 23:57:41 +0900

    수정

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -2,7 +2,6 @@
 var http = require('http');
 
 //http모듈로 서버를 생성
-//
 var server = http.createServer(function(request,response){
     response.writeHead(200, {'Content-Type':'text/html'});
     response.end('Hello node.js!!');
