933b9c2ec5fd3cb60cbbee1ac4bc5d76d8b672c2
LeeRyanghwa <iryanghwa@gmail.com>
2020-01-05 17:41:26 +0900

    commit

---
+++ mysql/connect.js
@@ -0,0 +1,10 @@
+console.log('connect.js start');
+
+var mysl =require('mysql');
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:''
+});
+
