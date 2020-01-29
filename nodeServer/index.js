004fd778d24b6882dfbd7282ce56774ae48d3555
LeeRyanghwa <iryanghwa@gmail.com>
2020-01-29 14:12:48 +0900

    commit

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 //http모듈로 서버를 생성
 var server = http.createServer(function(request,response){
-    response.writeHead(200, {'Content-Type':'text/html'});
+    response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
     response.write('<h1>시작</h1>')
     response.end('Hello node.js!');
     
