f4349fa7118c4394ae41301cfc6fd529b1885bb1
LeeRyanghwa <iryanghwa@gmail.com>
2020-01-30 17:35:32 +0900

    js

---
+++ example/filterEX.js
@@ -0,0 +1,6 @@
+const numbers = [2, 6, 7, 8, 1];
+const even = numbers.filter(function(x){
+    return x%2 == 0;
+});
+
+console.log(even);
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,11 +1,29 @@
 //http모듈 사용
 var http = require('http');
+var fs = require('fs');
+
+//요청한 url을 객체로 만들기 위해 url모듈 사용
+var url = require('url');
+//요청한 url 중에 Quert String을 객체로 만들기 위해 querystring 모듈 사용
+var querystring = require('querystring');
 
 //http모듈로 서버를 생성
 var server = http.createServer(function(request,response){
+
+    var parsedUrl = url.parse(request.url);
+
+    console.log(parsedUrl);
+    var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
+
+    console.log(parsedQuery.test);
+
+
     response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    response.write('<h1>시작</h1>')
-    response.end('Hello node.js!');
+    
+    //index.html 연결
+    if(request.method == 'GET' && request.url == '/'){
+        fs.createReadStream("./index.html").pipe(response);
+    }
     
 });
 //8080포트 서버실행
