773214517f9d263f577dca1ce9bd0b681ea9c2ff
LeeRyanghwa <iryanghwa@gmail.com>
2020-01-31 14:14:17 +0900

    js

--- example/filterEX.js
+++ example/filterEX.js
@@ -1,6 +1,20 @@
 const numbers = [2, 6, 7, 8, 1];
-const even = numbers.filter(function(x){
-    return x%2 == 0;
+
+//filter 함수를 이용하여 원하는 값 추출
+const even1 = numbers.filter(function(x){
+    if(x%2 == 0){
+        console.log(x + 'is even!');
+        return true;
+    }
 });
 
-console.log(even);
+console.log('=================');
+
+
+// function 대신 x => () 를 이용
+const even2 = numbers.filter(x => {
+    if(x%2 == 0){
+        console.log(x + 'is even!');
+        return true;
+    }
+});
