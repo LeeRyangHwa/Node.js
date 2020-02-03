a4f8575824ad29a536a9ba1f122f11726c431332
LeeRyanghwa <iryanghwa@gmail.com>
2020-02-03 21:59:01 +0900

    Map

--- example/MapEx.js
+++ example/MapEx.js
@@ -13,3 +13,14 @@ profiles.get('facebook');
 for(const entry of profiles){
     console.log(entry);
 }
+
+
+const tests = new Map();
+tests.set(() => 2+2, 4);
+tests.set(() => 2*2, 4);
+tests.set(() => 2/2, 1);
+
+for (const entry of tests){
+    console.log(entry[0]());
+    console.log((entry[0]() === entry[1]) ? 'PASS' : "FAIL");
+}
